a96587e00d9333079b91ae9d886df6264a6ce3d3
KuajinaiSS <kuajinaiss@gmail.com>
2023-12-08 19:49:28 -0300

    refactor: Remove unused code related to weather forecast

---
+++ Backend/MobileHub/Controllers/RepositoriesController.cs
@@ -0,0 +1,33 @@
+// TODO : colcoar token del David
+
+using DotNetEnv;
+using Microsoft.AspNetCore.Mvc;
+using Octokit;
+
+namespace MobileHub.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class RepositoriesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<GitHubCommit>>> Get() // para usar commits <GitHubCommit>, para repositorios <Repository>
+        {
+            Env.Load();
+
+            var client = new GitHubClient(new ProductHeaderValue("MobileHub"));
+            var myToken = Env.GetString("GITHUB_ACCESS_TOKEN");
+            var TokenCredential = new Credentials(myToken);
+
+            client.Credentials = TokenCredential;
+
+            // var repositories = (await client.Repository.GetAllForUser("KuajinaiSS")).ToList();
+            // return repositories;
+
+            var commits = (await client.Repository.Commit.GetAll("KuajinaiSS", "Taller_3_IDWM")).ToList();
+            return commits;
+
+        }
+    }
+} 
